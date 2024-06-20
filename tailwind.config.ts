02698241e15fc0e37438c00fb9cfc108a6440b1c
ronak1603 <ronakmalani2002@gmail.com>
2024-06-21 00:37:26 +0530

    updation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#FB8E0B",
+        iconColor: "#b9f1ba",
+        alert: {
+          error: "#F04438",
+          success: "#12B76A",
+        },
+      },
+      fontFamily: {
+        primary: "sans-serif",
       },
     },
   },
