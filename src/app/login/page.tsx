2eb588953775fc88236df9e3f209d8770ba4534b
ronak1603 <ronakmalani2002@gmail.com>
2024-06-21 00:38:23 +0530

    fixes acc to cliend and server components

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,22 +1,11 @@
-"use client";
+import Signin from "@/components/Login";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
-
-import { dashboardRoutes } from "@/utils/routes";
-import { fonts } from "@/utils/constants";
 import mobile from "../../assets/logo/mobile.svg";
-import { colors } from "@/utils/color-scheme";
-import logo from "../../assets/logo/logo.svg";
 
 const Login = () => {
-  const router = useRouter();
   return (
-    <div
-      className={`flex w-full h-screen bg-white font-[${fonts.primaryFont}]`}
-    >
-      <div
-        className={`flex flex-col w-full justify-center gap-16 items-center bg-[${colors.primary}]`}
-      >
+    <div className="flex w-full h-screen bg-white font-primary">
+      <div className="flex flex-col w-full justify-center gap-16 items-center bg-primary">
         <div className="flex flex-col gap-2 mt-4">
           <span className="flex flex-col text-center font-semibold flex-wrap break-words text-3xl ">
             Social media shared today,
@@ -25,52 +14,9 @@ const Login = () => {
             tomorrow or by location
           </span>
         </div>
-        <Image src={mobile} alt="login" />
+        <Image src={mobile} alt="Signin" />
       </div>
-      <div className="flex gap-4 items-center justify-center w-full">
-        <form>
-          <div className="flex flex-col justify-center gap-3 items-center">
-            <Image
-              src={logo}
-              alt="logo"
-              width={150}
-              height={100}
-              className="my-6"
-            />
-            {/* <span className="text-2xl my-6 font-semibold ">Create Account</span> */}
-            <div className="flex flex-col gap-1 p-1 items-start">
-              <p className="w-32 text-sm font-medium text-black">
-                <span>Email</span>
-              </p>
-              <div className="w-full flex flex-col">
-                <input
-                  type="text"
-                  placeholder="Enter your Email"
-                  className="w-96 h-12 px-2 border rounded outline-none border-gray-500"
-                />
-              </div>
-            </div>
-            <div className="flex flex-col gap-1 p-1 items-start">
-              <p className="w-32 text-sm font-medium text-black">
-                <span>Password</span>
-              </p>
-              <div className="w-full flex flex-col">
-                <input
-                  type="password"
-                  placeholder="Enter your Password"
-                  className="w-96 h-12 px-2 border rounded outline-none border-gray-500"
-                />
-              </div>
-            </div>
-            <div
-              onClick={() => router.push(dashboardRoutes.dashboard)}
-              className={`flex p-3 w-full mt-2 h-12 hover:shadow-md cursor-pointer rounded justify-center bg-[${colors.primary}]`}
-            >
-              <span className="text-sm font-medium text-center">Login</span>
-            </div>
-          </div>
-        </form>
-      </div>
+      <Signin />
     </div>
   );
 };
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,23 +1,17 @@
-"use client";
+import Register from "@/components/Register";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 
-import { fonts } from "@/utils/constants";
 import mobile from "../../assets/logo/mobile.svg";
-import { colors } from "@/utils/color-scheme";
-import logo from "../../assets/logo/logo.svg";
-import { dashboardRoutes } from "@/utils/routes";
 
-const Signup = () => {
-  const router = useRouter();
+const Signup = async () => {
+  const response = await fetch("https://api.github.com/users/ronak1603");
+  const data = await response.json();
+
+  console.log("#### data", data);
 
   return (
-    <div
-      className={`flex w-full h-screen bg-white font-[${fonts.primaryFont}]`}
-    >
-      <div
-        className={`flex flex-col w-full justify-center gap-16 items-center bg-[${colors.primary}]`}
-      >
+    <div className="flex w-full h-screen bg-white font-primary">
+      <div className="flex flex-col w-full justify-center gap-16 items-center bg-primary">
         <div className="flex flex-col gap-2 mt-4">
           <span className="flex flex-col text-center font-semibold flex-wrap break-words text-3xl ">
             Social media shared today,
@@ -26,64 +20,9 @@ const Signup = () => {
             tomorrow or by location
           </span>
         </div>
-        <Image src={mobile} alt="Signup" />
+        <Image src={mobile} alt="Register" />
       </div>
-      <div className="flex gap-4 items-center justify-center w-full">
-        <form>
-          <div className="flex flex-col justify-center gap-3 items-center">
-            <Image
-              src={logo}
-              alt="logo"
-              width={150}
-              height={100}
-              className="my-6"
-            />
-            {/* <span className="text-2xl my-6 font-semibold ">Create Account</span> */}
-            <div className="flex flex-col gap-1 p-1 items-start">
-              <p className="w-32 text-sm font-medium text-black">
-                <span>Name</span>
-              </p>
-              <div className="w-full flex flex-col">
-                <input
-                  type="text"
-                  placeholder="Enter your Name"
-                  className="w-96 h-12 px-2 border rounded outline-none border-gray-500"
-                />
-              </div>
-            </div>
-            <div className="flex flex-col gap-1 p-1 items-start">
-              <p className="w-32 text-sm font-medium text-black">
-                <span>Email</span>
-              </p>
-              <div className="w-full flex flex-col">
-                <input
-                  type="text"
-                  placeholder="Enter your Email"
-                  className="w-96 h-12 px-2 border rounded outline-none border-gray-500"
-                />
-              </div>
-            </div>
-            <div className="flex flex-col gap-1 p-1 items-start">
-              <p className="w-32 text-sm font-medium text-black">
-                <span>Password</span>
-              </p>
-              <div className="w-full flex flex-col">
-                <input
-                  type="password"
-                  placeholder="Enter your Password"
-                  className="w-96 h-12 px-2 border rounded outline-none border-gray-500"
-                />
-              </div>
-            </div>
-            <div
-              onClick={() => router.push(dashboardRoutes.dashboard)}
-              className={`flex p-3 w-full mt-2 h-12 hover:shadow-md cursor-pointer rounded justify-center bg-[${colors.primary}]`}
-            >
-              <span className="text-sm font-medium text-center">Register</span>
-            </div>
-          </div>
-        </form>
-      </div>
+      <Register />
     </div>
   );
 };
