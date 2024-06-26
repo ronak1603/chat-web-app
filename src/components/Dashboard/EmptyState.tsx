ace09582d1787009c2d638c7bf0efa5e62d134a8
ronak1603 <ronakmalani2002@gmail.com>
2024-06-26 12:26:05 +0530

    empty state

---
+++ src/components/Dashboard/EmptyState.tsx
@@ -0,0 +1,9 @@
+const EmptyState = () => {
+  return (
+    <div className="flex items-center justify-center h-full text-gray-500">
+      Select a chat to start messaging
+    </div>
+  );
+};
+
+export default EmptyState;
